ba2c7609a6decd3a05d20193a42ec17cda25510f
LocalVeganWitch <michelleannlockamy@gmail.com>
2026-07-19 00:16:32 -0400

    About page changes

---
+++ src/scripts/todaysquote.js
@@ -0,0 +1,5 @@
+const script = document.createElement("script");
+script.src = "https://www.brainyquote.com/link/quotebr.js";
+script.async = true;
+
+document.getElementById("daily-quote")?.appendChild(script);
